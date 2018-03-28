8ebd35378e67a38c1a7c3a0e5f39b933db74258d
gyba1998 <gyba1998@mail.ru>
2018-03-29 02:52:47 +0500

    Update
    CharacterController теперь наследуется.  Спрайты новые.

--- Game/CATlast/Assets/Scripts/CharacterControllerScript.cs
+++ Game/CATlast/Assets/Scripts/CharacterControllerScript.cs
@@ -1,13 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
 
-public class CharacterControllerScript : MonoBehaviour
+public class CharacterControllerScript : Character
 {
     //переменная для установки макс. скорости персонажа
-    public float maxSpeed = 10f;
+    public float maxSpeed = 5f;
     //переменная для определения направления персонажа вправо/влево
-    private bool isFacingRight = true;
-    //ссылка на компонент анимаций
-    private Animator anim;
+    
 
 
     //находится ли персонаж на земле или в прыжке?
@@ -25,17 +24,10 @@ public class CharacterControllerScript : MonoBehaviour
     /// <summary>
     /// Начальная инициализация
     /// </summary>
-	private void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
 
-    private Rigidbody2D rigi;
+   
 
-    private void Awake()
-    {
-        rigi = GetComponent<Rigidbody2D>();
-    }
+    
 
     /// <summary>
     /// Выполняем действия в методе FixedUpdate, т. к. в компоненте Animator персонажа
@@ -47,17 +39,17 @@ public class CharacterControllerScript : MonoBehaviour
         //определяем, на земле ли персонаж
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
         //устанавливаем соответствующую переменную в аниматоре
-        anim.SetBool("Ground", isGrounded);
+        animator.SetBool("Ground", isGrounded);
         //устанавливаем в аниматоре значение скорости взлета/падения
-        anim.SetFloat("vSpeed", rigi.velocity.y);
+        animator.SetFloat("vSpeed", rigidbody.velocity.y);
         //если персонаж в прыжке - выход из метода, чтобы не выполнялись действия, связанные с бегом
-       /* if (!isGrounded)
-            return;
-            */
+        /* if (!isGrounded)
+             return;
+             */
 
 
 
-   
+
         //используем Input.GetAxis для оси Х. метод возвращает значение оси в пределах от -1 до 1.
         //при стандартных настройках проекта 
         //-1 возвращается при нажатии на клавиатуре стрелки влево (или клавиши А),
@@ -67,18 +59,18 @@ public class CharacterControllerScript : MonoBehaviour
 
         //в компоненте анимаций изменяем значение параметра Speed на значение оси Х.
         //приэтом нам нужен модуль значения
-        anim.SetFloat("Speed", Mathf.Abs(move));
+        animator.SetFloat("Speed", Mathf.Abs(move));
 
         //обращаемся к компоненту персонажа RigidBody2D. задаем ему скорость по оси Х, 
         //равную значению оси Х умноженное на значение макс. скорости
-      
-        rigi.velocity = new Vector2(move * maxSpeed, rigi.velocity.y);
+
+        rigidbody.velocity = new Vector2(move * maxSpeed, rigidbody.velocity.y);
         //если нажали клавишу для перемещения вправо, а персонаж направлен влево
-        if (move > 0 && !isFacingRight)
+        if (move > 0 && !isFacedRight)
             //отражаем персонажа вправо
             Flip();
         //обратная ситуация. отражаем персонажа влево
-        else if  (move < 0 && isFacingRight)
+        else if (move < 0 && isFacedRight)
             Flip();
     }
 
@@ -88,7 +80,7 @@ public class CharacterControllerScript : MonoBehaviour
     private void Flip()
     {
         //меняем направление движения персонажа
-        isFacingRight = !isFacingRight;
+        isFacedRight = !isFacedRight;
         //получаем размеры персонажа
         Vector3 theScale = transform.localScale;
         //зеркально отражаем персонажа по оси Х
@@ -104,9 +96,9 @@ public class CharacterControllerScript : MonoBehaviour
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             //устанавливаем в аниматоре переменную в false
-            anim.SetBool("Ground", false);
+            animator.SetBool("Ground", false);
             //прикладываем силу вверх, чтобы персонаж подпрыгнул
-            rigi.AddForce(new Vector2(0, 600));
+            rigidbody.AddForce(new Vector2(0, 600));
         }
 
 
